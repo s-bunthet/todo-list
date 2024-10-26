860c81e678a65277ec5d831dc38d4e48b55ce6d5
Bunthet SAY <bunthet.say@presta.tessi.fr>
2024-10-27 00:17:12 +0200

    add task, delete task, validate input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import React, {useState} from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   //   list of the tasks
@@ -13,15 +16,20 @@ function App() {
 
   function addTask(input,id) {
       // control that input in not empty
-      if (input===""){
-          alert("Input can not be empty");
-          return;
+      if (input.trim()===""){
+          setValidated(true);
+      } else {
+          setValidated(false);
+
+          // Handle the "Add" action here
+          const updatedTasks = [...tasks];
+          updatedTasks.push({detail:input, id:id})
+          setTasks(updatedTasks);
+          setInput('');
+          setId(id+1);
       }
-      const updatedTasks = [...tasks];
-      updatedTasks.push({detail:input, id:id})
-      setTasks(updatedTasks);
-      setInput('');
-      setId(id+1);
+
+
   }
 
   function deleteTask(id) {
@@ -29,30 +37,55 @@ function App() {
       setTasks(updatedTasks);
   }
 
+const [validated, setValidated] = useState(false);
+
+
 
   return (
       <Container className={`App px-4`} fluid={`md`} >
           <Row className={`justify-content-center`}>
               <Col xs={12} md={6} className={`text-center`}>
-                  <h1>TODO LIST</h1>
-                  <form>
-                      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-                      <button type="button" onClick={()=>addTask(input,id)}>Add</button>
-                  </form>
+                  <h1 className={`text-primary fw-bold`}>TODO List</h1>
+                  <Form noValidate>
+                      <Row className={`justify-content-between`}>
+                          <Col md="11" className={`px-0`}>
+                              <Form.Group>
+                                  <Form.Control
+                                      required
+                                      type="text"
+                                      placeholder="Write your task here ..."
+                                      defaultValue=""
+                                      value={input}
+                                      onChange={(e) => setInput(e.target.value)}
+                                      className="mb-3 "
+                                      isInvalid={validated && input.trim() ===""}
+                                  />
+                                  <Form.Control.Feedback type="invalid">Task can not be empty!</Form.Control.Feedback>
+                              </Form.Group>
+                          </Col>
+                          <Col md="1" className={`px-0 mx-0`} >
+                              <Button  className="mb-1 btn-success" type="button" onClick={()=>addTask(input,id)}>Add</Button>
+                          </Col>
+                      </Row>
+
+                  </Form>
 
               </Col>
 
           </Row>
           <Row className={`justify-content-center`}>
-              <Col xs={12} md={6}>
+              <Col xs={12} md={6} className={`px-0 mx-0`}>
+                  <ListGroup as="ol" numbered>
                   {
                       tasks.map((task) => (
-                          <div key={task.id}>
-                              {task.detail}
-                              <button type="button" onClick={()=>deleteTask(task.id)}>Delete</button>
-                          </div>
+                          <ListGroup.Item  as="li" key={task.id} className="d-flex justify-content-between align-items-start p-1">
+                              <div className={`ms-2 me-auto`}>{task.detail}</div>
+                              <Button className={`btn-danger`} size="sm" type="button" onClick={() => deleteTask(task.id)}>Delete</Button>
+                          </ListGroup.Item>
+
                       ))
                   }
+                  </ListGroup>
               </Col>
           </Row>
       </Container>
