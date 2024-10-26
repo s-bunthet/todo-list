6cea41481d02a66325cb8ac433a73eb753aea2a4
Bunthet SAY <bunthet.say@presta.tessi.fr>
2024-10-26 16:26:47 +0200

    add task, delete task, view tasks list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import React, {useState} from "react";
 
 function App() {
+  //   list of the tasks
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+  const [id, setId] = useState(0);
+
+  function addTask(input,id) {
+      // control that input in not empty
+      if (input===""){
+          alert("Input can not be empty");
+          return;
+      }
+      const updatedTasks = [...tasks];
+      updatedTasks.push({detail:input, id:id})
+      setTasks(updatedTasks);
+      setInput('');
+      setId(id+1);
+  }
+
+  function deleteTask(id) {
+      const updatedTasks = tasks.filter(task => task.id !== id);
+      setTasks(updatedTasks);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container className={`App px-4`} fluid={`md`} >
+          <Row className={`justify-content-center`}>
+              <Col xs={12} md={6} className={`text-center`}>
+                  <h1>TODO LIST</h1>
+                  <form>
+                      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+                      <button type="button" onClick={()=>addTask(input,id)}>Add</button>
+                  </form>
+
+              </Col>
+
+          </Row>
+          <Row className={`justify-content-center`}>
+              <Col xs={12} md={6}>
+                  {
+                      tasks.map((task) => (
+                          <div key={task.id}>
+                              {task.detail}
+                              <button type="button" onClick={()=>deleteTask(task.id)}>Delete</button>
+                          </div>
+                      ))
+                  }
+              </Col>
+          </Row>
+      </Container>
+
   );
 }
 
